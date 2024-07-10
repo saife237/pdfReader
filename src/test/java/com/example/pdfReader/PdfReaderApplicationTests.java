738afb533ba998fe7ae625319f9280b92ae9cb36
saife237 <saifedev@gmail.com>
2024-07-10 17:40:23 +0530

    first commit

---
+++ src/test/java/com/example/pdfReader/PdfReaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdfReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfReaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
